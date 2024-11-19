9cd6e34572ae3f25e9b0abdde105fb8313c50d85
Italo R. Dev <italorodridev@gmail.com>
2024-11-19 14:08:30 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Testes Ionic 7',
+  webDir: 'www'
+};
+
+export default config;
